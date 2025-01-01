fd836edd21986136e468e7b3505a567b5e92655e
darce-radic <132355796+darce-radic@users.noreply.github.com>
2025-01-01 18:57:59 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import ui_management
 from subscriptions import process_uploaded_file, enrich_merchant_data
