94cc267942876530c7cd5453d52e93a4e6dd1178
darce-radic <132355796+darce-radic@users.noreply.github.com>
2025-01-01 23:12:47 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+
+__import__('pysqlite3')
 import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from supabase import create_client, Client
 import ui_management
